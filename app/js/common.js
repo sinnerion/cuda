11d463177ab8784af29bd038c37c0a856c35a63a
Sinnerion <a.dergachow@gmail.com>
2016-08-30 15:11:09 +0300

    main-nav finished again; portfolio have bugs again

--- app/js/common.js
+++ app/js/common.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
     // Portfolio more projects // This script sometimes gives bugs
     $('.more-projects').hide();
     $('.portfolio-btn').on('click', function(){
-        // $(this).preventDefault();
+        $(this).preventDefault();
         $('.more-projects').toggle('slow');
         $('.default-projects').toggle('slow');
     });
